6772fc8c3a210e7b494f9c26793c9786c0b67c1f
kayla <fasreaperearnest@gmail.com>
2023-06-24 15:05:18 -0700

    use raylib instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,20 @@
-// SDL
-#include <SDL2/SDL.h>
+#include <raylib.h>
 
-// std
-#include <cstdio>
+const int WIDTH = 800;
+const int HEIGHT = 450;
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+int main() {
+    InitWindow(WIDTH, HEIGHT, "Alcheminer");
 
-int main (int argc, char* args[]) {
-    SDL_Window* window = nullptr;
-    SDL_Surface* screenSurface = nullptr;
+    while (!WindowShouldClose()) {
+        BeginDrawing();
 
-    // initialise SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
-        return 1;
+            ClearBackground(RAYWHITE);
+
+        EndDrawing();
     }
 
-    // make window
-    window = SDL_CreateWindow(
-            "Alcheminer",
-            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-            SCREEN_WIDTH, SCREEN_HEIGHT,
-            SDL_WINDOW_SHOWN
-            );
-    if (window == nullptr) {
-        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    // """game loop"""
-    screenSurface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(2000);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    CloseWindow();
 
     return 0;
 }
