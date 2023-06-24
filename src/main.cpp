27d6f8dca6cc7144e2e2327378ba59b6ef143aff
kayla <fasreaperearnest@gmail.com>
2023-06-24 12:32:21 -0700

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
+// SDL
 #include <SDL2/SDL.h>
+
+// std
 #include <cstdio>
 
 #define SCREEN_WIDTH 640
@@ -8,23 +11,25 @@ int main (int argc, char* args[]) {
     SDL_Window* window = nullptr;
     SDL_Surface* screenSurface = nullptr;
 
+    // initialise SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
         return 1;
     }
 
+    // make window
     window = SDL_CreateWindow(
             "Alcheminer",
             SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
             SCREEN_WIDTH, SCREEN_HEIGHT,
             SDL_WINDOW_SHOWN
             );
-
     if (window == nullptr) {
         fprintf(stderr, "could not create window: %s\n", SDL_GetError());
         return 1;
     }
 
+    // """game loop"""
     screenSurface = SDL_GetWindowSurface(window);
     SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
     SDL_UpdateWindowSurface(window);
