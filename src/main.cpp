8474d1f70a172890f5aa7d98b4cb03878242b3b2
kayla <fasreaperearnest@gmail.com>
2023-06-24 10:47:13 -0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,36 @@
+#include <SDL2/SDL.h>
+#include <cstdio>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+int main (int argc, char* args[]) {
+    SDL_Window* window = nullptr;
+    SDL_Surface* screenSurface = nullptr;
+
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    window = SDL_CreateWindow(
+            "Alcheminer",
+            SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+            SCREEN_WIDTH, SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN
+            );
+
+    if (window == nullptr) {
+        fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
