86fa5f8cb3eeae1195b96210f4ffdfed9929ba45
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-06-25 00:29:28 -0700

    get rid of debu messages because raylib already makes them

--- src/assetMan/Sprite.cpp
+++ src/assetMan/Sprite.cpp
@@ -4,16 +4,12 @@
 
 #include "Sprite.h"
 
-#include <iostream>
-
 namespace AssetMan {
 
 	int Sprite::LoadAsset(std::string relPath) {
 		// concatenate relative path to a c-string
 		std::string path = ASSETS + relPath;
 		const char* cPath = path.c_str();
-
-		std::cout << "Loading asset:" << cPath << std::endl;
 
 		texture = LoadTexture(cPath);
 
