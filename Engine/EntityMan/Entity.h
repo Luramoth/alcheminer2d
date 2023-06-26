ed8cd52fe2a0b1d86d0a59e8a74d5b93d6501783
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-06-26 16:28:40 -0700

    heafty documentation

--- Engine/EntityMan/Entity.h
+++ Engine/EntityMan/Entity.h
@@ -1,6 +1,22 @@
 //
 // Created by kayla on 6/25/23.
 //
+//// ENTITY: Base class for all objects that exist in-world.
+////
+//// all entities contain a list of components that describe
+//// what the entity is and how each thread should handle
+//// the entity individually
+////
+//// the Awake() function gets run by the main thread once a particular entity is instanced into a world
+////
+//// the Start() function gets run by the main thread once all entities in a scene are instanced
+////
+//// the Tick() function gets run by the physics thread every time a tick goes by, bound by the games global tickrate
+////
+//// the Frame() function gets run by the Render thread before the next frame is about to render, handy to run logic
+//// that will actually change things visually on screen such as movement
+////
+//// the End() function gets run by the main thread once a particular entity is about to get deleted
 
 #ifndef ALCHEMINER2D_ENTITY_H
 #define ALCHEMINER2D_ENTITY_H
@@ -12,7 +28,12 @@ namespace EntityMan {
 	class Entity {
 	public:
 		std::list<ComponentMan::Component> Components;
+
+		virtual void Awake();
+		virtual void Start();
 		virtual void Tick();
+		virtual void Frame();
+		virtual void End();
 	};
 
 }// EntityMan
